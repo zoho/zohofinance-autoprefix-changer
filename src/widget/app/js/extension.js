0be6c74c861421abccfcb651203401631916b533
Saravana <102511096+Saravanaaa@users.noreply.github.com>
2022-04-20 20:17:14 +0530

    Initial Commit

--- src/widget/app/js/extension.js
+++ src/widget/app/js/extension.js
@@ -1,9 +1,7 @@
 // Elements
-const select = document.querySelectorAll('.select');
-const select2 = document.querySelectorAll('.inputarea');
 const preview = document.getElementsByClassName('preview');
 const pluginpreview = document.querySelectorAll('.Pluginpreview');
-const checkbox = document.getElementById("checkbox");
+const reset_checkbox = document.getElementById('checkbox');
 
 const inv_preview = document.querySelector('#inv-preview');
 const est_preview = document.querySelector('#est-preview');
@@ -12,258 +10,267 @@ const po_preview = document.querySelector('#purchase-preview');
 
 const inv_select = document.querySelector('#inv-select');
 const est_select = document.querySelector('#est-select');
-const so_select  = document.querySelector('#sales-select');
-const po_select  = document.querySelector('#purchase-select');
+const so_select = document.querySelector('#sales-select');
+const po_select = document.querySelector('#purchase-select');
 
 // Config
-
-const global_field_Prefix_Formate = 'vl__utpsb_prefixformate';
-const global_field_Number_Reset = 'vl__utpsb_number_reset';
-
+const placeholders =['vl__utpsb_prefixformate','vl__utpsb_number_reset'];
 const booksConnection = 'zohobookssettingsall';
 
-let date;
+let month = new Date().getMonth() + 1;
+let year_4 = new Date().getFullYear();
+let year_2 = new Date().getFullYear().toString().substr(-2);
+let month_in_str = {
+  '1': 'Jan',
+  '2': 'Feb',
+  '3': 'Mar',
+  '4': 'Apr',
+  '5': 'may',
+  '6': 'Jun',
+  '7': 'Jul',
+  '8': 'Aug',
+  '9': 'Sep',
+  '10': 'Oct',
+  '11': 'Nov',
+  '12': 'Dec',
+}
 
-// dict  used to insert drop down 
+// dict  used to insert drop down
 
-let datePreview=
-    {
-        'MM-YYYY':"Apr-2022-",
-        'MM-YY':'Apr-22-',
-        "YYYY-MM":"2022-Apr-",
-        "YY-MM":"22-Apr-",
-        "mm-YYYY":"11-2022-",
-        "mm-YY":"11-22-",
-        "YYYY-mm":"2022-11-",
-        "YY-mm":"22-11-",
-        "YYYY":"2022-",
-        "YY":"22-",
-        "MM":"Apr-",
-        "mm":"11-",
-    }
+let datePreview = {
 
-//Hash map for dict (dataPreview) 
+  'MM-YYYY': month_in_str[month] + '-' + year_4 + '-',
+  'MM-YY': month_in_str[month] + '-' + year_2 + '-',
+  'YYYY-MM': year_4 + '-' + month_in_str[month] + '-',
+  'YY-MM': year_2 + '-' + month_in_str[month] + '-',
+  'mm-YYYY': month + '-' + year_4 + '-',
+  'mm-YY': month + '-' + year_2 + '-',
+  'YYYY-mm': year_4 + '-' + month + '-',
+  'YY-mm': year_2 + '-' + month + '-',
+  'YYYY': year_4 + '-',
+  'YY': year_2 + '-',
+  'MM': month_in_str[month] + '-',
+  'mm': month + '-',
+}
 
-let dateFormat=[
+//Hash map for dict (dataPreview)
 
-    { date:"MM-YYYY" },
-    { date:"MM-YY"   },
-    { date:"YYYY-MM" },
-    { date:"YY-MM"   },
-    { date:"mm-YYYY" },
-    { date:"mm-YY"   },
-    { date:"YYYY-mm" },
-    { date:"YY-mm"   },
-    { date:"YYYY"    },
-    { date:"YY"      },
-    { date:"MM"      },
-    { date:"mm"      }
+let dateFormats = [
 
+  'MM-YYYY',
+  'MM-YY',
+  'YYYY-MM',
+  'YY-MM',
+  'mm-YYYY',
+  'mm-YY',
+  'YYYY-mm',
+  'YY-mm',
+  'YYYY',
+  'YY',
+  'MM',
+  'mm',
 ]
 
 //Update the data in drop-down
 
-let j=0
-while(j<select.length){
-    for(var i of dateFormat){
-        option=document.createElement('option');
-        option.value=i.date;
-        option.text=option.value.charAt(0).toUpperCase()+option.value.slice(1);
-        select[j].appendChild(option);
-    }
-    j++;
-    
+const append_drop_down = (select) => {
+  for (let format in dateFormats) {
+    option = document.createElement('option')
+    option.value = dateFormats[format]
+    option.text = option.value.charAt(0).toUpperCase() + option.value.slice(1)
+    select.appendChild(option)
+  }
 }
 
-// display-preview 
+append_drop_down(inv_select);
+append_drop_down(est_select);
+append_drop_down(so_select);
+append_drop_down(po_select);
 
-function change(){
-    
-    let i=0;
-    while(i<select2.length){
-        let selector=select[i].options[select[i].selectedIndex];
-        if(selector.value=='none'){
-            i++;
-        }
-        else{
 
-            date=datePreview[selector.value]
-            preview[i].innerText= select2[i].value+"-" + date;
-            console.log(preview[i].innerText);
-            let id = pluginpreview[i].id;
-            console.log(id);
-            if((preview[i].innerText).length > 13){
-               
-                document.getElementById(id).classList.add("active");
+// display-preview
 
-                break;
-            }
-            else{
-                document.getElementById(id).classList.remove("active");
-            }
-            i++;
-        }
-
+function change( tag ) {
+   let parent=tag.parentElement;
+   let parentvalue=tag.parentElement.nodeName;
+    if((parentvalue)!='TR'){
+        change(parent);
     }
-}
-
-// get the value of selected dropdown
-
-const selecteddropdown =(select,value)=>{
-
-    for (i=0;i<select.length;i++){
-        if ( select[i].value == value) {
-            select.selectedIndex=i;
-            break;
+    else{
+        let input_value = parent.getElementsByTagName('input')[0].value;
+        let select_value = parent.getElementsByTagName('select')[0].value;
+        if(select_value != "none"){
+            let preview = parent.getElementsByTagName('span');
+            preview[0].innerText = input_value+"-"+datePreview[select_value];
         }
     }
 }
 
 window.onload = () => {
-    ZFAPPS.extension.init().then( async() => {
-        ZFAPPS.invoke('RESIZE', { width: '800px', height: '360px' });
-        
+  ZFAPPS.extension.init().then(async () => {
+    ZFAPPS.invoke('RESIZE', { width: '800px', height: '360px' })
+
         let { organization } = await ZFAPPS.get('organization');
-        await getglobal(organization,global_field_single_line);
-        let entity=
-        {
-            "Invoice": { 
-                "Prefix" : "INV" , "Date": " "
-            },
-            "Estimate": { 
-                "Prefix" : "EST" , "Date": " "
-            },
-            "SalesOrder":{ 
-                "Prefix" : "SO" , "Date": " "
-            }, 
-            "PurchaseOrder":{
-                "Prefix": "PO" ,"Date": " "
-            }
-        }
 
-        hook = async(App) => {
+        await getglobal(organization, placeholders[0]);
+        await getglobal(organization,placeholders[1]);
+        let excess_value=[];
+        App.instance.on('ON_SETTINGS_WIDGET_PRE_SAVE', () => {
+            for(let index=0; index=preview.length ; index++){
+                let length = preview[index].length;
+                if(length<=13){
 
-            App.instance.on("ON_SETTINGS_WIDGET_PRE_SAVE", async() => {
-                var checked = "no";
-                if (checkbox.checked) {
-                  var checked = "yes";
+                    hook();
                 }
-                entity["Invoice"].Prefix = (inv_preview.textContent).split('-')[0] || "INV" ;
-                entity['Invoice'].Date = inv_select.value;
-                entity['Estimate'].Prefix =(est_preview.textContent).split('-')[0] || "EST";
-                entity['Estimate'].Date = est_select.value ;
-                entity['SalesOrder'].Prefix = (so_preview.textContent).split('-')[0] || "SO";
-                entity['SalesOrder'].Date = so_select.value;
-                entity['PurchaseOrder'].Prefix = (po_preview.textContent).split('-')[0] || "PO";
-                entity['PurchaseOrder'].Date = po_select.value;
-        
-                await updateGlobalVariables(organization,entity,global_field_Prefix_Formate);
-                await updateGlobalVariables(organization,checked,global_field_Number_Reset);
-        
-            });
-        }
-        
-    });
-   
+                else{
+                    excess_value.push(index);
+                }   
+            }
+            for (let index in excess_value){
+                alert("%d in the preview element not exceed 13 charatcers ",index);
+            }
+        })
+    })
 }
 
-
+const hook = async () => {
+    let entity = {
+        Invoice: {
+            Prefix: 'INV',
+            Date: ' ',
+        },
+        Estimate: {
+            Prefix: 'EST',
+            Date: ' ',
+        },
+        SalesOrder: {
+            Prefix: 'SO',
+            Date: ' ',
+        },
+        PurchaseOrder: {
+            Prefix: 'PO',
+            Date: ' ',
+        },
+    }
+    var checked = 'no'
+    if (reset_checkbox.checked) {
+        var checked = 'yes'
+    }
+    entity['Invoice'].Prefix = inv_preview.textContent.split('-')[0] || 'INV'
+    entity['Invoice'].Date = inv_select.value
+    entity['Estimate'].Prefix = est_preview.textContent.split('-')[0] || 'EST'
+    entity['Estimate'].Date = est_select.value
+    entity['SalesOrder'].Prefix = so_preview.textContent.split('-')[0] || 'SO'
+    entity['SalesOrder'].Date = so_select.value
+    entity['PurchaseOrder'].Prefix =po_preview.textContent.split('-')[0] || 'PO'
+    entity['PurchaseOrder'].Date = po_select.value
+            
+    //update the values in globalfields
+    await updateGlobalVariables(organization,entity,placeholders[0]);
+            
+    //update the checkbox fields
+    await updateGlobalVariables(organization,checked,placeholders[1]);
+}
 
 // Api call to get the globalfields
 
-const getglobal =async(organization,global_field_id) => {
+const getglobal = async (organization, placeholder) => {
 
-    let inv_date;
-    let po_date;
-    let est_date;
-    let so_date;
-
-    let globaldata={
-        url:
-            "https://books.zoho.com/api/v3/settings/orgvariables/" +global_field_id,
-        method:'GET',
-        url_query:[
-            {
-                key:'organization_id',
-                value:organization.organization_id,
-            },
+    const booksAPIPrefix = `https://books.zoho${ organization.data_center_extension || '.com' }/api/v3`;
+    let globaldata = {
+        url: booksAPIPrefix + '/settings/orgvariables/' + placeholder,
+        method: 'GET',
+        url_query: [
+        {
+            key: 'organization_id',
+            value: organization.organization_id,
+        },
         ],
-        header:[{"key":"Content-Type","value":"application/json"}],
-        connection_link_name:booksConnection
-    };
-    try{
-        let { data: { body } } = await ZFAPPS.request(globaldata);
+        header: [{ key: 'Content-Type', value: 'application/json' }],
+        connection_link_name: booksConnection,
+    }
+    try 
+    {
+        let checkbox_value; 
+        let {data: { body } } = await ZFAPPS.request(globaldata);
         let { orgvariable: { value } } = JSON.parse(body);
-        let { Invoice:{
+        let {
+        Invoice: { Prefix: inv_prefix, Date: inv_date_format },
+        Estimate: { Prefix: est_prefix, Date: est_date_format },
+        SalesOrder: { Prefix: so_prefix, Date: so_date_format },
+        PurchaseOrder: { Prefix: po_prefix, Date: po_date_format },
+        } = JSON.parse(value) || checkbox_value ;
+        
+        let inv_date = datePreview[inv_date_format];
 
-            Prefix: inv_prefix ,Date: inv_date_format },
-            Estimate: { Prefix: est_prefix , Date: est_date_format },
-            SalesOrder:{ Prefix: so_prefix , Date: so_date_format }, 
-            PurchaseOrder:{ Prefix: po_prefix, Date: po_date_format}
-            
-        }=JSON.parse(value);
-
-        if ((inv_date = datePreview[inv_date_format])!=undefined) {
-
-            inv_preview.innerText = inv_prefix+"-"+inv_date;
-            selecteddropdown(inv_select,inv_date_format);
-
+        if (inv_date  != undefined) {
+        inv_preview.innerText = inv_prefix + '-' + inv_date;
+        let index = dateFormats.indexOf(inv_date_format);
+        inv_select.selectedIndex = index;
         }
 
-        if ((est_date = datePreview[est_date_format])!=undefined) {
+        let est_date = datePreview[est_date_format];
 
-            est_preview.innerText = est_prefix + "-" + est_date;
-            selecteddropdown(est_select,est_date_format);
+        if (est_date  != undefined) {
+        est_preview.innerText = est_prefix + '-' + est_date;
+        let index = dateFormats.indexOf(est_date_format);
+        est_select.selectedIndex = index;
+        
         }
 
-        if ((so_date = datePreview[so_date_format])!=undefined){
+        let so_date = datePreview[so_date_format];
 
-            so_preview.innerText =so_prefix + "-" + so_date;
-            selecteddropdown(so_select,so_date_format);
+        if ( so_date  != undefined) {
+        so_preview.innerText = so_prefix + '-' + so_date;
+        let index = dateFormats.indexOf(so_date_format);
+        so_select.selectedIndex = index;
         }
 
-        if ((po_date = datePreview[po_date_format])!=undefined){
+        let po_date = datePreview[po_date_format];
 
-            po_preview.innerText = po_prefix + "-" + po_date;
-            selecteddropdown(po_select,po_date_format);
+        if (po_date != undefined) {
+        po_preview.innerText = po_prefix + '-' + po_date
+        let index = dateFormats.indexOf(po_date_format);
+        po_select.select.selectedIndex = index;
         }
-
-    }catch(err){
+        if (checkbox_value=="yes"){
+        reset_checkbox.checked= true;
+        }
+    } catch (err) {
         console.log(err);
     }
 }
 
 //api call for update the global fields
 
-const updateGlobalVariables = async(organization,value,global_field_id) => {
+const updateGlobalVariables = async (organization, value, placeholder) => {
+  var data = { value: value }
+  const booksAPIPrefix = `https://books.zoho${ organization.data_center_extension || '.com' }/api/v3`;
+  var options = {
 
-    var data = { value:value };
-    var options = {
-        url:
-          "https://books.zoho.com/api/v3/settings/orgvariables/" + global_field_id,
-        method: "PUT",
-        url_query: [
-          {
-            key: "organization_id",
-            value: organization.organization_id,
-          },
-        ],
-        body: {
-          mode: "formdata",
-          formdata: [
-            {
-              key: "JSONString",
-              value: JSON.stringify(data),
-            },
-          ],
+    url: booksAPIPrefix+'/settings/orgvariables/' + placeholder,
+    method: 'PUT',
+    url_query: [
+      {
+        key: 'organization_id',
+        value: organization.organization_id,
+      },
+    ],
+    body: {
+      mode: 'formdata',
+      formdata: [
+        {
+          key: 'JSONString',
+          value: JSON.stringify(data),
         },
-        connection_link_name: booksConnection
-      };
-      try{
-          let value =await ZFAPPS.request(options);
-          console.log(value);
-      }catch(err){
-          console.log(err);
-    }
-
+      ],
+    },
+    connection_link_name: booksConnection,
+  }
+  try {
+    let value = await ZFAPPS.request(options)
+    console.log(value)
+  } catch (err) {
+    console.log(err)
+  }
 }
